15611e1a155bf73b866d4f96c855084e196b9f8b
Elan Roth <elanroth@sas.upenn.edu>
2026-03-08 00:44:38 -0500

    make it deployable

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/YOUR-REPO-NAME/',
+  base: '/lamarcusAldridge/',
   server: {
     port: 3001,
   },
